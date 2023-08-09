76fd55e324d78337637fde4f7fffbdd1e459cbb0
Vildis <tubettajat10@gmail.com>
2023-08-09 18:08:17 +0300

    Add missing classes

---
+++ DungeonGen/Point.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace DungeonGenerator
+{
+	// Token: 0x02000006 RID: 6
+	public struct Point
+	{
+		// Token: 0x0600000F RID: 15 RVA: 0x00002288 File Offset: 0x00000488
+		public Point(int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+
+		// Token: 0x06000010 RID: 16 RVA: 0x00002298 File Offset: 0x00000498
+		public Point(double x, double y)
+		{
+			this = new Point((int)Math.Round(x), (int)Math.Round(y));
+		}
+
+		// Token: 0x06000011 RID: 17 RVA: 0x000022AE File Offset: 0x000004AE
+		public override string ToString()
+		{
+			return string.Format("({0}, {1})", this.X, this.Y);
+		}
+
+		// Token: 0x04000007 RID: 7
+		public static readonly Point Zero = default(Point);
+
+		// Token: 0x04000008 RID: 8
+		public readonly int X;
+
+		// Token: 0x04000009 RID: 9
+		public readonly int Y;
+	}
+}
+
