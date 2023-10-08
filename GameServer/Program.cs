0541b52a900482ab15711a1ed2ab19d6c3f71c3b
Slendergo <Survivalistrivalist@gmail.com>
2023-10-08 10:10:19 +0100

    Xml Changes

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -5,6 +5,9 @@ using GameServer.realm;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Org.BouncyCastle.Asn1.Cms;
+using System.Text;
+using System.Xml.Linq;
 
 namespace GameServer;
 
@@ -17,6 +20,148 @@ internal class Program {
     internal static Database Database;
 
     private static void Main(string[] args) {
+
+        //var files = Directory.GetFiles("Test", "*.xml");
+        //foreach(var file in files)
+        //{
+        //    var info = new FileInfo(file);
+
+        //    var xml = XElement.Load(file);
+
+        //    var tag = xml.Elements("Object").Select(_ =>
+        //    {
+        //        {
+        //            var e = _.Element("HitSound");
+        //            if (e != null)
+        //            {
+        //                var f = e.Value;
+        //                while (f != null)
+        //                {
+        //                    var index = f.IndexOf('_');
+        //                    if (index == -1)
+        //                        break;
+
+        //                    var sb = new StringBuilder(f);
+        //                    if (f.Contains("/"))
+        //                        sb[f.IndexOf("/") + 1] = char.ToUpper(f[f.IndexOf("/") + 1]);
+        //                    else
+        //                        sb[0] = char.ToUpper(f[0]);
+        //                    sb[index] = '#';
+        //                    index++;
+        //                    sb[index] = char.ToUpper(f[index]);
+        //                    f = sb.ToString();
+        //                }
+        //                e.Value = f.Replace("#", "");
+        //            }
+        //        }
+
+        //        {
+        //            var e = _.Element("DeathSound");
+        //            if (e != null)
+        //            {
+        //                var f = e.Value;
+        //                while (f != null)
+        //                {
+        //                    var index = f.IndexOf('_');
+        //                    if (index == -1)
+        //                        break;
+
+        //                    var sb = new StringBuilder(f);
+        //                    if (f.Contains("/"))
+        //                        sb[f.IndexOf("/") + 1] = char.ToUpper(f[f.IndexOf("/") + 1]);
+        //                    else
+        //                        sb[0] = char.ToUpper(f[0]);
+        //                    sb[index] = '#';
+        //                    index++;
+        //                    sb[index] = char.ToUpper(f[index]);
+        //                    f = sb.ToString();
+        //                }
+        //                e.Value = f.Replace("#", "");
+        //            }
+        //        }
+
+        //        {
+        //            var e = _.Element("Sound");
+        //            if (e != null)
+        //            {
+        //                var f = e.Value;
+        //                while (f != null)
+        //                {
+        //                    var index = f.IndexOf('_');
+        //                    if (index == -1)
+        //                        break;
+
+        //                    var sb = new StringBuilder(f);
+        //                    if (f.Contains("/"))
+        //                        sb[f.IndexOf("/") + 1] = char.ToUpper(f[f.IndexOf("/") + 1]);
+        //                    else
+        //                        sb[0] = char.ToUpper(f[0]);
+        //                    sb[index] = '#';
+        //                    index++;
+        //                    sb[index] = char.ToUpper(f[index]);
+        //                    f = sb.ToString();
+        //                }
+        //                e.Value = f.Replace("#", "");
+        //            }
+        //        }
+
+
+        //        {
+        //            var e = _.Element("OldSound");
+        //            if (e != null)
+        //            {
+        //                var f = e.Value;
+        //                while (f != null)
+        //                {
+        //                    var index = f.IndexOf('_');
+        //                    if (index == -1)
+        //                        break;
+
+        //                    var sb = new StringBuilder(f);
+        //                    if (f.Contains("/"))
+        //                        sb[f.IndexOf("/") + 1] = char.ToUpper(f[f.IndexOf("/") + 1]);
+        //                    else
+        //                        sb[0] = char.ToUpper(f[0]);
+        //                    sb[index] = '#';
+        //                    index++;
+        //                    sb[index] = char.ToUpper(f[index]);
+        //                    f = sb.ToString();
+        //                }
+        //                e.Value = f.Replace("#", "");
+        //            }
+        //        }
+
+        //        return _;
+        //    }).ToList();
+
+        //    if (tag.Count != 0)
+        //    {
+        //        xml = new XElement("Objects", tag);
+        //        File.Delete(file);
+        //        using var fs = new FileStream(file, FileMode.OpenOrCreate);
+        //        xml.Save(fs);
+        //    }
+
+        //    used for Renaming Files
+        //    var f = info.Name;
+        //    while (true)
+        //    {
+        //        var index = f.IndexOf('_');
+        //        if (index == -1)
+        //            break;
+
+        //        var sb = new StringBuilder(f);
+        //        sb[0] = char.ToUpper(f[0]);
+        //        sb[index] = '#';
+        //        index++;
+        //        sb[index] = char.ToUpper(f[index]);
+        //        f = sb.ToString();
+        //    }
+
+        //    var path = $"Test/{f.Replace("#", "")}";
+        //    File.Move(info.FullName, path);
+        //}
+
         AppDomain.CurrentDomain.UnhandledException += LogUnhandledException;
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
         Thread.CurrentThread.Name = "Entry";
